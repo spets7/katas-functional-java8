da20ed0b4ff7baa8cee12537e2ebe37cca6e7248
Houcine <ouadallah.h@gmail.com>
2018-11-10 11:53:34 +0100

    kata 2 add filter and map tand return list of int over 5

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -16,7 +17,8 @@ import java.util.Map;
 public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(1, 2, 3);
+        return movies.stream().filter((movie)->{
+            return movie.getRating()>0;
+        }).map(movie -> movie.getRating().intValue()).collect(Collectors.toList());
     }
 }
